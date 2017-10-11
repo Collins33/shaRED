777757095c6533d00e04953fda956a03920258f4
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-11 15:07:12 +0300

    I have added the log in and sign up activities and intents to move from each other

--- app/src/main/java/com/example/root/shared/Sign_in.java
+++ app/src/main/java/com/example/root/shared/Sign_in.java
@@ -1,13 +1,29 @@
 package com.example.root.shared;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class Sign_in extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class Sign_in extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.signUpTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        Intent intent=new Intent(getApplicationContext(),Sign_up.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/root/shared/Sign_up.java
+++ app/src/main/java/com/example/root/shared/Sign_up.java
@@ -1,13 +1,30 @@
 package com.example.root.shared;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class Sign_up extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+public class Sign_up extends AppCompatActivity implements View.OnClickListener{
+@Bind(R.id.loginTextView) TextView mLogIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+
+        mLogIn.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view){
+        if(view == mLogIn){
+            Intent intent=new Intent(getApplicationContext(),Sign_in.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
