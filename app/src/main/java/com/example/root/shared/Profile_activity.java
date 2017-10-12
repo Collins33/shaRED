4fabfb7fbc5b914d830a29a1eec6b1b49f08326a
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-12 11:48:12 +0300

    I have added the ability to fill a form to create a profile and have it saved to firebase

--- app/src/main/java/com/example/root/shared/Profile_activity.java
+++ app/src/main/java/com/example/root/shared/Profile_activity.java
@@ -2,12 +2,53 @@ package com.example.root.shared;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Profile_activity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class Profile_activity extends AppCompatActivity implements View.OnClickListener{
+   @Bind(R.id.submitAccount) Button mButton;
+    @Bind(R.id.dateOfBirthView) EditText mBithday;
+    @Bind(R.id.sexEditView) EditText mGender;
+    @Bind(R.id.bloodTypeEditView) EditText mBloodType;
+    @Bind(R.id.residenceEditView) EditText mResidence;
+    @Bind(R.id.emailEditView) EditText mEmail;
+    @Bind(R.id.nameEditView) EditText mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_activity);
+        ButterKnife.bind(this);
+        mButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view){
+        if(view == mButton){
+            saveProfile();
+        }
+    }
+    public void saveProfile(){
+        String name=mName.getText().toString().trim();
+        String dateOfBirth=mBithday.getText().toString().trim();
+        String sex=mGender.getText().toString().trim();
+        String bloodType=mBloodType.getText().toString().trim();
+        String residence=mResidence.getText().toString().trim();
+        String email=mEmail.getText().toString().trim();
+        Account account=new Account(name,dateOfBirth,sex,bloodType,residence,email);
+
+        DatabaseReference profileRef= FirebaseDatabase
+                .getInstance()
+                .getReference(Constants.FIREBASE_CHILD_PROFILE);
+        profileRef.push().setValue(account);
+        Toast.makeText(getApplicationContext(),"profile saved",Toast.LENGTH_LONG).show();
     }
 }
