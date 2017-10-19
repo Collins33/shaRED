0e67bc0ea53e7b53e053b16b63c9dcb0b7e249ef
sammwas <samson.m.njuguna@gmail.com>
2017-10-19 11:19:10 +0300

    Another pull and merge process

--- app/src/main/java/com/example/root/shared/DashboardActivity.java
+++ app/src/main/java/com/example/root/shared/DashboardActivity.java
@@ -1,22 +1,26 @@
 package com.example.root.shared;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 
 import java.util.ArrayList;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements  View.OnClickListener{
     private ArrayList<Category> categories;
     private RecyclerView recyclerView;
     private DashboardItemAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private CardView mCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,7 @@ public class DashboardActivity extends AppCompatActivity {
 
 
         recyclerView = (RecyclerView) findViewById(R.id.dashboardRecycler);
+        mCardView=(CardView) findViewById(R.id.card_view);
 
         categories = new ArrayList<>();
         adapter = new DashboardItemAdapter(this, categories);
@@ -34,6 +39,8 @@ public class DashboardActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10),true));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        mCardView.setOnClickListener(this);
+
 
         categories = new ArrayList<Category>();
         for(int i=0;i<= 3;i++) {
@@ -43,6 +50,21 @@ public class DashboardActivity extends AppCompatActivity {
         adapter = new DashboardItemAdapter(getApplicationContext(),categories);
         recyclerView.setAdapter(adapter);
     }
+    @Override
+    public void onClick(View view){
+        if(view == mCardView){
+            getNewIntent();
+        }
+    }
+    public void getNewIntent(){
+        categories = new ArrayList<Category>();
+        for(int i=0;i<= 3;i++) {
+            categories.add(new Category(MyData.categorys[i],MyData.images[i]));
+
+        }
+
+    }
+
 
     public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
 
--- app/src/main/java/com/example/root/shared/DashboardItemAdapter.java
+++ app/src/main/java/com/example/root/shared/DashboardItemAdapter.java
@@ -1,7 +1,9 @@
 package com.example.root.shared;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,9 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Locale;
+
+import static android.R.attr.category;
+import static android.R.attr.start;
 
 /**
  * Created by mwas on 10/16/17.
@@ -30,7 +35,7 @@ public class DashboardItemAdapter extends RecyclerView.Adapter<DashboardItemAdap
         return mContext;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener  {
         //list all the views that are in the card view layout
         public ImageView imageIcon;
         public TextView iconTitle;
@@ -39,8 +44,31 @@ public class DashboardItemAdapter extends RecyclerView.Adapter<DashboardItemAdap
             super(itemView);
             imageIcon = (ImageView) itemView.findViewById(R.id.person_photo);
             iconTitle = (TextView) itemView.findViewById(R.id.person_name);
+            itemView.setOnClickListener(this);
         }
+        @Override
+        public void onClick(View v) {
+
+
+            categoryData = new ArrayList<Category>();
+            for(int i = 0; i<= 3; i++){
+                categoryData.add(new Category(MyData.categorys[i],MyData.images[i]));
+
+            }
+
+            if(categoryData.indexOf(0) == 0){
+                Intent intent = new Intent(getContext(),BloodDriveActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                mContext.startActivity(intent);
+            }
+            Log.v("checkout","" +String.valueOf(categoryData.get(0)) );
+
+
+        }
+
     }
+
+
 
     // Usually involves inflating a layout from XML and returning the holder
     @Override
